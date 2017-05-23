f93f4105871007b77123cc47ce3cea93218ca7e1
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-05-23 15:06:13 +0200

    feat(snek): draw trivial snek, yeey

--- game.js
+++ game.js
@@ -6,21 +6,54 @@
     const MAP_WIDTH = snake.MAP_WIDTH;
     const MAP_HEIGHT = snake.MAP_HEIGHT;
 
-    const gameMap = generateRandomMap();
+    const gameSnake = [
+      {x: 21, y: 11},
+      {x: 21, y: 15}
+    ];
 
-    function generateRandomMap() {
+    const gameMap = generateMap();
+
+    function generateMap() {
       const length = MAP_WIDTH * MAP_HEIGHT;
-      const generateRandomTile = 
-        () => Math.floor(Math.random() * 2);
-      return Array.from({length}, generateRandomTile);
+      const map = Array.from({length}, () => 0);
+      for (let i = 0; i < gameSnake.length - 1; ++i) {
+        const snakeIntersect = gameSnake.slice(i, i + 2);
+        console.log(snakeIntersect);
+        if (snakeIntersect[0].x === snakeIntersect[1].x) {
+          const x = snakeIntersect[0].x;
+          for (let y = snakeIntersect[0].y; y <= snakeIntersect[1].y; y++) {
+            map[y * MAP_WIDTH + x] = 1;
+          } 
+        } else {
+          // Horizontal
+        }
+      }
+      return map;
+    }
+
+    function generateSnakeBodyPart(_, i) {
+      const coords = index2coordinates(i);
+
+      return gameSnake
+         .some(point => compareCoords(point, coords));
+    }
+
+    function compareCoords(a, b) {
+      return (a.x === b.x && a.y === b.y);
+    }
+
+    function index2coordinates(index) {
+      const x = index % MAP_WIDTH;
+      const y = Math.floor(index / MAP_WIDTH);
+    
+      return {x, y};
     }
 
     function render() {
       gameMap.forEach((e, i) => {
         if (e) {
-          const x = i % MAP_WIDTH;
-          const y = Math.floor(i / MAP_WIDTH);
-          context.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+          const coords = index2coordinates(i);
+          context.fillRect(coords.x * TILE_SIZE, coords.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
         }
       });
     }
